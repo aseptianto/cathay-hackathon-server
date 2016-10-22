ed7a0143081cf265b57106706ba2088a29e6685c
Ivan Gondoprastowo <laurentius.ivan@gmail.com>
2016-10-23 00:00:18 +0800

    models - exist or create for many to many

--- app/ProfilesLocations.php
+++ app/ProfilesLocations.php
@@ -8,7 +8,7 @@ class ProfilesLocations extends Model
 {
     protected $table = 'profiles_locations';
 
-    public static function existsOrCreate($profile, $education) {
+    public static function existsOrCreate($profile, $location) {
         $profiles_locations =
             ProfilesLocations::where('profile_id', $profile['id'])->where('location_id', $location['id'])->get();
 
@@ -17,7 +17,7 @@ class ProfilesLocations extends Model
         } else {
             $profiles_locations = new ProfilesLocations();
             $profiles_locations->profile_id = $profile['id'];
-            $profiles_locations->education_id = $location['id'];
+            $profiles_locations->location_id = $location['id'];
             $profiles_locations->save();
         }
     }
